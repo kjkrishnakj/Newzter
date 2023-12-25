818df41914fa48364bb31b4f79c30d9c2261737a
Krishna Jaiswal <78219949+kjkrishnakj@users.noreply.github.com>
2023-12-25 15:48:20 +0530

    descriptive commit message

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
 import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
-
+import News from './components/News';
+import About from './components/About';
+import LoadingBar from 'react-top-loading-bar';
+import {
+  Routes,
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
 export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <Navbar/>
-      </div>
+
+apiKey  = process.env.REACT_APP_NEWS_API;
+state={progress : 0}
+setProgress= (progress)=>{
+  this.setState({progress : progress})
+}
+render() {
+  return (
+    <Router>
+
+        <Navbar />
+        <div>
+          <LoadingBar
+            color='#f11946'
+            height={3}
+            progress={this.state.progress}
+          />
+          
+        </div>
+        <Routes>
+
+
+          <Route path="/about" element={<About />} />
+          <Route path="/buisness" element={<News apiKey = {this.apiKey} setProgress =  {this.setProgress} key='buisness' pageSize={15} country="in" category="business" />}></Route>
+          <Route path="/entertainment" element={<News apiKey = {this.apiKey} setProgress =  {this.setProgress} key='entertainment' pageSize={15} country="in" category="entertainment" />}></Route>
+          <Route path="/" element={<News apiKey = {this.apiKey} setProgress =  {this.setProgress} key='Free Daily Newz!' pageSize={15} country="in" category="general" />}></Route>
+          <Route path="/health" element={<News apiKey = {this.apiKey} setProgress =  {this.setProgress} key='health' pageSize={15} country="in" category="health" />}></Route>
+          <Route path="/science" element={<News apiKey = {this.apiKey} setProgress =  {this.setProgress} key='science' pageSize={15} country="in" category="science" />}></Route>
+          <Route path="/sports" element={<News apiKey = {this.apiKey} setProgress =  {this.setProgress} key='sports' pageSize={15} country="in" category="sports" />}></Route>
+          <Route path="/technology" element={<News apiKey = {this.apiKey} setProgress =  {this.setProgress} key='technology' pageSize={15} country="in" category="technology" />}></Route>
+
+        </Routes>
+
+      </Router>
     )
   }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,30 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Navbar extends Component {
     render() {
         return (
             <>
-                <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            
+                <nav className="navbar fixed-top navbar-expand-lg navbar-dark  bg-primary">
                     <div className="container-fluid">
-                        <a className="navbar-brand" to="/">Navbar</a>
+                        <Link className="navbar-brand" to="/"><strong>Newzter</strong></Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" to="/">Home</a>
+                                    <Link className="nav-link" aria-current="page" to="/">Home</Link>
                                 </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" to="/">About</a>
-                                </li>
+                                <li className="nav-item"><Link className="nav-link" to="/about">About</Link></li>
+                                <li className="nav-item"><Link className="nav-link" to="/buisness">Buisness</Link></li>
+                                <li className="nav-item"><Link className="nav-link" to="/entertainment ">Entertainment</Link></li>
+                                <li className="nav-item"><Link className="nav-link" to="/health">Health</Link></li>
+                                <li className="nav-item"><Link className="nav-link" to="/science">Science</Link></li>
+                                <li className="nav-item"><Link className="nav-link" to="/sports">Sports</Link></li>
+                                <li className="nav-item"><Link className="nav-link" to="/technology">Technology</Link></li>
+                                
                                 
                             </ul>
 
